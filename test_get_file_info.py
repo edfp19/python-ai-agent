e9459dfe2c007639d0d14f83dad2adfd7f040dae
Ever <78941506+edfp19@users.noreply.github.com>
2026-01-12 17:15:25 +0000

    Second commit

---
+++ test_get_file_info.py
@@ -0,0 +1,5 @@
+from functions import get_files_info as gfi
+gfi.get_files_info("calculator", ".")
+gfi.get_files_info("calculator", "pkg")
+gfi.get_files_info("calculator", "/bin")
+gfi.get_files_info("calculator", "../")
